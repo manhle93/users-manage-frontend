6559655e0d8634c8d0390d2d3bab153e3e5d6d8a
ManhLe <zerokisss@gmail.com>
2021-11-23 13:54:59 +0700

    fix print

--- src/pages/Customers/management/print.vue
+++ src/pages/Customers/management/print.vue
@@ -1,11 +1,12 @@
 <template>
-  <div style="height: 100%; width: 100%;">
+  <div style="height: 100%; width: 100%">
     <table
       style="
         width: 100%;
         max-height: 100%;
         border: 1px solid black;
         border-collapse: collapse;
+        table-layout: fixed;
       "
     >
       <tr
@@ -22,19 +23,46 @@
             vertical-align: top !important;
             position: relative;
             padding: 20px;
+            word-wrap: break-word;
           "
           v-for="item in items"
           :key="item.id"
         >
           <div style="position: absolute; bottom: 20px; right: 20px">
-            担当者 {{ item.user_id }} {{ item.user ? item.user.company_name : "" }}
+            担当者 {{ item.user_id }}
+            {{ item.user ? item.user.company_name : "" }}
           </div>
           <div style="width: 100%" v-if="item.user_id">
             <div>
-              <div style="font-family:MS P明朝,MS 明朝, serif">〒 {{ item.postal_code }}</div>
-              <div style="font-family:MS P明朝,MS 明朝, serif">{{ item.address }}</div>
-              <div style="font-weight: bold; font-size:22px !important;font-family:HG行書体,HGP行書体,cursive">{{ item.company_name }} {{ item.representative_name ? "" : "御中"}}</div>
-              <div style="font-weight: bold; font-size:22px !important;font-family:HG行書体,HGP行書体,cursive">{{ item.representative_name ? item.representative_name + ' 様' : ""}} </div>
+              <div style="font-family: MS P明朝, MS 明朝, serif">
+                〒 {{ item.postal_code }}
+              </div>
+              <div style="font-family: MS P明朝, MS 明朝, serif">
+                {{ item.address }}
+              </div>
+              <div
+                style="
+                  font-weight: bold;
+                  font-size: 22px !important;
+                  font-family: HG行書体, HGP行書体, cursive;
+                "
+              >
+                {{ item.company_name }}
+                {{ item.representative_name ? "" : "御中" }}
+              </div>
+              <div
+                style="
+                  font-weight: bold;
+                  font-size: 22px !important;
+                  font-family: HG行書体, HGP行書体, cursive;
+                "
+              >
+                {{
+                  item.representative_name
+                    ? item.representative_name + " 様"
+                    : ""
+                }}
+              </div>
             </div>
           </div>
           <div v-else></div>
@@ -64,7 +92,7 @@ export default {
   }),
   watch: {
     data(val) {
-      this.tableData = []
+      this.tableData = [];
       const data = [...val];
       if (data.length % 2 == 1) {
         data.push({});
@@ -74,17 +102,24 @@ export default {
       }
     },
   },
-  mounted() {
-
-  },
+  mounted() {},
 };
 </script>
 
 <style>
-@page { size: auto;  margin: 0mm; }
+@page {
+  size: auto;
+  margin: 0mm;
+}
 .d-flex {
   display: flex;
   flex-direction: row;
+}
+table {
+  table-layout: fixed;
+}
+td {
+  word-wrap: break-word;
 }
 .c-flex {
   display: flex;
@@ -97,5 +132,4 @@ export default {
   float: left;
   margin: 5px;
 }
-
 </style>
